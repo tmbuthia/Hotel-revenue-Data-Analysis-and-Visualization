a848026d0291461a43ca6137719804381a43eb99
tmbuthia <128447688+tmbuthia@users.noreply.github.com>
2024-12-10 18:45:16 -0800

    Add files via upload

---
+++ SSMSHOTELQUERY.sql
@@ -0,0 +1,16 @@
+
+WITH hotel AS
+(
+SELECT *  FROM [Projects].[dbo].[historical revenue 2018]
+UNION
+SELECT *  FROM [Projects].[dbo].[historical revenue 2019]
+UNION
+SELECT *  FROM [Projects].[dbo].[historical revenue 2020]
+)
+
+SELECT
+	arrival_date_year,
+	hotel,
+    SUM((CAST(stays_in_week_nights AS INT) + CAST(stays_in_weekend_nights AS INT)) * CAST(adr AS decimal(10,2))) AS revenue
+FROM hotel
+GROUP BY arrival_date_year,	hotel;
